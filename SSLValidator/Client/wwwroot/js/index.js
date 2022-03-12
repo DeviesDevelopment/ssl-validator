71c3e0119cdc27735ff33497480e7477a12e41a4
gadapunkfun <robert.matijevic@devies.se>
2022-03-12 19:04:50 +0100

    ✨ init

---
+++ SSLValidator/Client/wwwroot/js/index.js
@@ -0,0 +1,10 @@
+﻿export function showPrompt(message) {
+	return prompt(message, 'Type anything here');
+}
+
+document.addEventListener("keypress", (e) => {
+	if (e.key === 'Enter') {
+		var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+		myModal.toggle();
+	}
+})
---
+++ SSLValidator/Server/Controllers/AssessedSSLCertificateController.cs
@@ -0,0 +1,25 @@
+﻿using SSLValidator.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSLValidator.Server.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class AssessedSSLCertificateController : ControllerBase
+	{
+		private readonly ILogger<AssessedSSLCertificateController> _logger;
+
+		public AssessedSSLCertificateController(ILogger<AssessedSSLCertificateController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<AssessedSSLCertification> Get()
+		{
+			return Enumerable.Range(1, 10)
+				.Select(index => new AssessedSSLCertification(Random.Shared.Next(1, 55)))
+				.ToArray();
+		}
+	}
+}
