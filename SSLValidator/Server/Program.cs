a2da2e247380c970898c09329dcead6f68c7f45b
gadapunkfun <robert.matijevic@devies.se>
2022-03-17 11:05:27 +0100

    added redis support

--- SSLValidator/Server/Program.cs
+++ SSLValidator/Server/Program.cs
@@ -6,19 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+	opt.Configuration = builder.Configuration.GetValue<string>("redisKey");
+	opt.InstanceName = "sslValidator_";
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
+	app.UseWebAssemblyDebugging();
 }
 else
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
