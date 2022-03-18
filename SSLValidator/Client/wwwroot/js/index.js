8508d726785a15a9b3a8076b3af1ae7d3febae48
gadapunkfun <robert.matijevic@devies.se>
2022-03-18 12:55:12 +0100

    added method to set background colour

--- SSLValidator/Client/wwwroot/js/index.js
+++ SSLValidator/Client/wwwroot/js/index.js
@@ -2,6 +2,10 @@
 	return prompt(message, 'Type anything here');
 }
 
+export function setBackgroundColour(threatLevelClass) {
+	document.body.className = threatLevelClass;
+}
+
 document.addEventListener("keypress", (e) => {
 	if (e.key === 'Enter') {
 		var myModal = new bootstrap.Modal(document.getElementById("domainModalToggle"));
