8f4276cde7993e5239d84fc4f2a9af21e804a5c0
gadapunkfun <robert.matijevic@devies.se>
2022-03-18 12:37:16 +0100

    increased cache lifetime

--- SSLValidator/Server/Extensions/DistributedCacheExtension.cs
+++ SSLValidator/Server/Extensions/DistributedCacheExtension.cs
@@ -9,8 +9,8 @@ namespace SSLValidator.Server.Extensions
 		public static async Task SetRecordAsync<T>(this IDistributedCache cache, string recordId, T data, TimeSpan? absoluteExpireTime = null)
 		{
 			var options = new DistributedCacheEntryOptions();
-			options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromMinutes(30);
-			options.SlidingExpiration = TimeSpan.FromMinutes(30);
+			options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromDays(2);
+			options.SlidingExpiration = TimeSpan.FromDays(2);
 			var jsonData = JsonSerializer.Serialize(data);
 			await cache.SetStringAsync(recordId, jsonData, options);
 		}
@@ -18,7 +18,7 @@ namespace SSLValidator.Server.Extensions
 		public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
 		{
 			var jsonData = await cache.GetStringAsync(recordId);
-			if (jsonData is null)
+			if (string.IsNullOrWhiteSpace(jsonData))
 			{
 				return default(T);
 			}
