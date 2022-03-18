8e4963e572839ebbdd17f78c53d1197e0f2efcde
gadapunkfun <robert.matijevic@devies.se>
2022-03-18 10:39:27 +0100

    added post endpoint

--- SSLValidator/Server/Controllers/DomainController.cs
+++ SSLValidator/Server/Controllers/DomainController.cs
@@ -1,17 +1,36 @@
-﻿using SSLValidator.Shared;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Distributed;
+
+using SSLValidator.Shared;
+using SSLValidator.Server.Hubs;
+using SSLValidator.Server.Services;
+using SSLValidator.Server.Extensions;
 
 namespace SSLValidator.Server.Controllers
 {
 	[ApiController]
-	[Route("[controller]")]
+	[Route("[controller]/[action]")]
 	public class DomainController : ControllerBase
 	{
 		private readonly ILogger<DomainController> _logger;
 
-		public DomainController(ILogger<DomainController> logger)
+		private readonly IDistributedCache _cache;
+
+		private readonly IHubContext<DomainHub> _domainHubContext;
+
+		public DomainController(ILogger<DomainController> logger, IDistributedCache cache, IHubContext<DomainHub> domainHubContext)
 		{
 			_logger = logger;
+			_cache = cache;
+			_domainHubContext = domainHubContext;
+		}
+
 		[HttpGet("{sessionId}")]
 		public async Task<ActionResult<List<Domain>>> Get(string sessionId)
 		{
@@ -29,7 +48,44 @@ namespace SSLValidator.Server.Controllers
 		}
 
 		[HttpGet]
-		public IEnumerable<Domain> Get()
+		public ActionResult<string> GetSessionId()
+		{
+			return Ok(RandomString());
+		}
+
+		[HttpPost("{sessionId}")]
+		public async Task<ActionResult> Post(string sessionId, [FromBody] DomainPayload payload)
+		{
+			var isURLValid = payload.URL.Contains("https");
+
+			if (!ModelState.IsValid || !isURLValid)
+			{
+				return BadRequest();
+			}
+
+			if (string.IsNullOrWhiteSpace(sessionId))
+			{
+				return BadRequest("No session id");
+			}
+
+			var domains = await _cache.GetRecordAsync<List<Domain>>(sessionId + "-domains");
+
+			if (domains is null)
+			{
+				domains = new List<Domain>();
+			}
+
+			var expirationDate = await GetSSLCertificateExpirationDate.GetDaysUntilExpirationAsync(payload.URL);
+			var newDomain = new Domain(payload.DomainName, payload.URL, expirationDate);
+
+			domains.Add(newDomain);
+			await _cache.SetRecordAsync(sessionId + "-domains", domains);
+
+			await _domainHubContext.Clients.All.SendAsync("ReceiveCurrentDomains", domains);
+			
+			return Ok();
+		}
+
 		public static string RandomString()
 		{
 			var random = new Random();
---
+++ SSLValidator/Shared/DomainPayload.cs
@@ -0,0 +1,9 @@
+namespace SSLValidator.Shared
+{
+	public class DomainPayload
+	{
+		public string DomainName { get; set; }
+
+		public string URL { get; set; }
+	}
+}
