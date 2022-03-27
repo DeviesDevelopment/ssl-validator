8306296f17e9a839f845aee20f26f162e617ecaf
gadapunkfun <robert.matijevic@devies.se>
2022-03-27 17:47:27 +0200

    added a process of removing "add domain modal" after adding a domain

--- SSLValidator/Client/wwwroot/js/index.js
+++ SSLValidator/Client/wwwroot/js/index.js
@@ -2,6 +2,15 @@
 	document.body.className = threatLevelClass;
 }
 
+export function toggleAddDomainModal() {
+	let modal = window.addDomainModal;
+	if (modal === undefined) {
+		window.addDomainModal = new bootstrap.Modal(document.getElementById("addDomainModalToggle"));
+		modal = window.addDomainModal;
+	}
+	modal.toggle();
+}
+
 document.addEventListener("keypress", (e) => {
 	if (e.key === 'Enter') {
 		let modal = window.domainModal;
