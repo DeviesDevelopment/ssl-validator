6afd840e7d275e57dfbad33b6f12b9621d5e1c0e
gadapunkfun <robert.matijevic@devies.se>
2022-03-30 00:22:37 +0200

    🏭 continued development on UpdateDomains hosted service job

--- SSLValidator/Server/Services/UpdateDomains.cs
+++ SSLValidator/Server/Services/UpdateDomains.cs
@@ -5,6 +5,8 @@ using SSLValidator.Shared;
 using SSLValidator.Server.Hubs;
 using SSLValidator.Server.Extensions;
 
+using System.Text.Json;
+
 namespace SSLValidator.Server.Services
 {
 	public class UpdateDomains : BackgroundService
@@ -41,24 +43,37 @@ namespace SSLValidator.Server.Services
 			try
 			{
 				var m = await StackExchange.Redis.ConnectionMultiplexer.ConnectAsync(_redisConnection);
-				var keys = m.GetServer("localhost", 5003).Keys().ToList();
+				var parsedRedisConnectionString = _redisConnection.Substring(0, _redisConnection.IndexOf(","));
+				var splitRedisConnectionString = parsedRedisConnectionString.Split(":");
+
+				if (!int.TryParse(splitRedisConnectionString[1], out var redisConnectionStringPort)) throw new Exception("Redis connection string invalid");
+
+				var keys = m.GetServer(splitRedisConnectionString[0], redisConnectionStringPort).Keys().ToList();
 				var domains = new List<Domain>();
+
 				if (keys.Any())
 				{
-					keys.ForEach(async (key) =>
+					var parsedKeys = keys.Select(key => key.ToString().Replace("sslValidator_", string.Empty));
+					var cachedDomains = (await Task.WhenAll<byte[]>(parsedKeys.Select(parsedKey => _cache.GetAsync(parsedKey)))).ToList();
+					var cachedDomainsWithKey = parsedKeys.Select((key, index) => (key, cachedDomains[index])).ToList();
+
+					cachedDomainsWithKey.ForEach(domain =>
 					{
-						domains.Add(await _cache.GetRecordAsync<Domain>(key));
+						var parsedDomains = JsonSerializer.Deserialize<List<Domain>>(domain.Item2);
+						if (parsedDomains is not null)
+						{
+							domains.AddRange(parsedDomains);
+						}
 					});
-					domains.ForEach(async (domain) =>
-					{
-						domain.DaysUntilExpiration = await GetSSLCertificateExpirationDate.GetDaysUntilExpirationAsync(domain.Url);
-					});
+
+					await Task.WhenAll(parsedKeys.Select((key, index) => _cache.SetRecordAsync(key, domains[index])));
+
+					await _domainHubContext.Clients.All.SendAsync("ReceiveCurrentDomains", domains);
 				}
 			}
 			catch (Exception)
 			{
-
-				throw;
+				throw new Exception("Something went wrong running background service to update domains");
 			}
 		}
 	}
